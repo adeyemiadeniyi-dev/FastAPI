b11968b8693c3f3f3f6c2330e61b025a20e32db6
adeyemiadeniyi-dev <adeyemiadeniyi97@gmail.com>
2026-03-09 22:28:00 +0100

    update database.py to load from .env

--- app/database.py
+++ app/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from app.config import settings
+import os
 
-DATABASE_URL = settings.database_url
+DATABASE_URL = os.getenv("DATABASE_URL", settings.database_url)
 
 engine = create_engine(DATABASE_URL, echo=True)
 
