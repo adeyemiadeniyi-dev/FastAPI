e7e1058e699cb433c3194a6ea09d4fffc2fa95ae
adeyemiadeniyi-dev <adeyemiadeniyi97@gmail.com>
2026-03-14 08:55:08 +0100

    fix oauth token path

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -8,7 +8,7 @@ router = APIRouter(
     tags=["Authentication"]
 )
 
-@router.post("/", status_code=status.HTTP_200_OK, response_model=schemas.Token)
+@router.post("/login", status_code=status.HTTP_200_OK, response_model=schemas.Token)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
     if not user:
