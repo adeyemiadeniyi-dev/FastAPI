14913ba7ded6a50e01fa3dba282a8b0d73394980
adeyemiadeniyi-dev <adeyemiadeniyi97@gmail.com>
2026-03-14 08:33:53 +0100

    fix oauth token path

--- app/oauth.py
+++ app/oauth.py
@@ -11,7 +11,7 @@ SECRET_KEY = settings.secret_key
 ALGORITHM = settings.algorithm
 ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 
 def create_access_token(data: dict[str, str]) -> str:
